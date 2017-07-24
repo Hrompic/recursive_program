1ffe1b57db2113b33ddf63759afe24a9c3f27fe6
Hrompic <semenxx@gmail.com>
2017-07-24 19:21:28 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+        int cycles = 0;
+        char filename[32];
+        if(argc >= 2) cycles = atoi(argv[2]);
+        sprintf(filename,""
+
+}
+~
